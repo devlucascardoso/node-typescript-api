48f14ff3c538679e96f1bd01ac25fb8f74c28859
Lucas Cardoso <lucascardoso946@gmail.com>
2021-10-13 17:37:10 -0300

    Finish the user module integration

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,26 +1,34 @@
 import bcrypt from 'bcrypt';
 import config from 'config';
 import jwt from 'jsonwebtoken';
+import { User } from '@src/models/user';
 
-export default class AuthService {
-
-public static async hashPassword(
-  password: string,
-  salt = 10
-): Promise<string> {
-  return await bcrypt.hash(password, salt);
+//version of the user that is send to via API and decoded from the Json Web Token
+export interface DecodedUser extends Omit<User, '_id'> {
+  id: string;
 }
 
-public static async comparePasswords(
-  password: string, 
-  hashedPassword: string
-  ): Promise<boolean> {
-  return await bcrypt.compare(password, hashedPassword);
+export default class AuthService {
+  public static async hashPassword(
+    password: string,
+    salt = 10
+  ): Promise<string> {
+    return await bcrypt.hash(password, salt);
   }
-  
-public static generateToken(payload: object): string {
+  public static async comparePasswords(
+    password: string,
+    hashedPassword: string
+  ): Promise<boolean> {
+    return await bcrypt.compare(password, hashedPassword);
+  }
+  public static generateToken(payload: object): string {
     return jwt.sign(payload, config.get('App.auth.key'), {
       expiresIn: config.get('App.auth.tokenExpiresIn'),
     });
   }
+
+  public static decodeToken(token: string): DecodedUser {
+    return jwt.verify(token, config.get('App.auth.key')) as DecodedUser;
+  }
+
 }
