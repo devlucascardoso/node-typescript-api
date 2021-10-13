82e23eb69d6f66855b9a0b9f2718c387d74984ff
Lucas Cardoso <lucascardoso946@gmail.com>
2021-10-13 12:40:27 -0300

    Add authenticate passwords and generate token

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,4 +1,6 @@
 import bcrypt from 'bcrypt';
+import config from 'config';
+import jwt from 'jsonwebtoken';
 
 export default class AuthService {
 
@@ -14,5 +16,11 @@ public static async comparePasswords(
   hashedPassword: string
   ): Promise<boolean> {
   return await bcrypt.compare(password, hashedPassword);
+  }
+  
+public static generateToken(payload: object): string {
+    return jwt.sign(payload, config.get('App.auth.key'), {
+      expiresIn: config.get('App.auth.tokenExpiresIn'),
+    });
   }
 }
